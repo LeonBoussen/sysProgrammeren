3e0dc9284e435141aa8b584fcc05f0cc52a3ed95
Leon Boussen <leon.boussen@hotmail.com>
2021-11-05 09:36:52 +0100

    les 1

--- Les 1/ConsoleApp1/Program.cs
+++ Les 1/ConsoleApp1/Program.cs
@@ -4,9 +4,45 @@ namespace ConsoleApp1
 {
     class Program
     {
+        private class Enemy
+        {
+            public int enemyHealth = 500;
+
+            public Enemy(int _enemyHealth)
+            {
+                enemyHealth = _enemyHealth;
+            }
+
+            public void TakeDamage(int damage)
+            {
+                if (enemyHealth > 0)
+                {
+                    enemyHealth -= damage;
+                    if (enemyHealth <= 0)
+                    {
+                        Console.WriteLine("man he dead as helll!!!!!");
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Enemy enemy = new Enemy(1);
+
+            Random rnd = new Random();
+
+            while (1 == 1)
+            {
+                enemy.TakeDamage(rnd.Next(69, 420));
+
+                Console.WriteLine("Enemy health: " + enemy.enemyHealth);
+
+                if (enemy.enemyHealth <= 0)
+                {
+                    break;
+                }
+            }
         }
     }
 }
