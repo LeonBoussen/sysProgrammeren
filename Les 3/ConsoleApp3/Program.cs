ad256ff8e1d285a50b1003c07de83755fab66757
Leon Boussen <leon.boussen@hotmail.com>
2021-11-05 09:42:13 +0100

    Les 3

--- Les 3/ConsoleApp3/Program.cs
+++ Les 3/ConsoleApp3/Program.cs
@@ -4,9 +4,34 @@ namespace ConsoleApp3
 {
     class Program
     {
+        private class titel
+        {
+
+        }
+
+        private class Kamer
+        {
+            private titel[][] titel;
+
+            public Kamer(int width, int height)
+            {
+                this.titel = new titel[height][];
+
+                for (int i = 0; i < height; i++)
+                {
+                    this.titel[i] = new titel[width];
+
+                    for (int j = 0; j < width; j++)
+                    {
+                        this.titel[i][j] = new titel();
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kamer kammer = new Kamer(69,420);
         }
     }
 }
