c0ac68b65ed57acefd0b2eab8a507443d85ab039
Leon Boussen <leon.boussen@hotmail.com>
2021-11-05 09:52:48 +0100

    Les 5

--- Les 5/ConsoleApp5/Program.cs
+++ Les 5/ConsoleApp5/Program.cs
@@ -6,7 +6,84 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player fpsplayer = new Player(420);
+
+            Money money = new Money(10);
+
+            HealPotion healPotion = new HealPotion(30);
+
+            Console.WriteLine("De speler heeft money: " + fpsplayer.pMoney);
+            Console.WriteLine("De speler heeft levens: " + fpsplayer.pHealth);
+        }
+
+        public class Player
+        {
+            private int startingHealth;
+
+            public int pMoney = 0;
+            public int pHealth = 0;
+
+            public Player(int startingHealth)
+            {
+                this.startingHealth = startingHealth;
+                pHealth = startingHealth;
+            }
+
+            public void AddMoney(int amount)
+            {
+                pMoney += amount;
+            }
+
+            public void AddHealth(int amount)
+            {
+                pHealth += amount;
+                if (pHealth > startingHealth) pHealth = startingHealth;
+            }
+        }
+
+        public abstract class Equipment
+        {
+            public virtual void Equip()
+            {
+                Console.WriteLine("Item equipped");
+            }
+        }
+
+
+        public class Armor : Equipment
+        {
+            public override void Equip()
+            {
+                base.Equip();
+            }
+        }
+
+        public class Weapon : Equipment
+        {
+            public override void Equip()
+            {
+                base.Equip();
+            }
+        }
+
+        public class Money
+        {
+            public int pMoney;
+
+            public Money(int money)
+            {
+                this.pMoney = money;
+            }
+        }
+
+        public class HealPotion
+        {
+            public int heal;
+
+            public HealPotion(int heal)
+            {
+                this.heal = heal;
+            }
         }
     }
 }
